9554ed2cb89fbcf323b17cebf85293acc5a65608
Ensar Baltas <ensarbaltas@Ensar-MBP.fritz.box>
2019-07-26 16:23:54 +0200

    1.odeve

--- 1.odev.py
+++ 1.odev.py
@@ -1,27 +1,29 @@
 ##-Kullanicidan aklindan 0-100 araliginda bir sayi tutmasini isteyin.
 
 import random
-print("Aklinizdan 0-100 araliginda bir sayi tutunuz")
-bilgisayar=random.randint(0,100)
-bilgisayar1=0
-bilgisayar2=0
-print(bilgisayar)
+print("""Aklinizdan 0-100 araliginda bir sayi tutunuz, tuttugunuz sayi
+      tahminimden kuccukse (-) ye, buyukse (+)  ya basiniz, sayiyi bildimse
+      (bildin) yaziniz.""")
 
+tahmin1=0
+tahmin2=101
+tahminler=[]
 
 while True:
-    kul=input(" ")
-    sayac=0
-    sira=0
-    if kul=="-":
-        sayac-=10
-        bilgisayar1=random.randint(sayac,bilgisayar)
-        print(bilgisayar1)
-        
-    if kul=="+":
-        sira+=10
-        bilgisayar2=random.randint(sira,bilgisayar)
-        print(bilgisayar2)
-        
+    
+    bilgisayar=random.randint(tahmin1,tahmin2)
+    print("Tahminim",bilgisayar)
+    kullanici=input("kullanici degerlendirmesi :")
+    kullanici=kullanici.lower()   
+    tahminler.append(bilgisayar)
+    print(tahminler)
+    if kullanici=="-":
+        tahmin2=bilgisayar    
+    elif kullanici=="+":
+        tahmin1=bilgisayar
+    elif kullanici=="bildin": 
+        print("Oyun icin tesekkurler")
+        break
         continue
             
 
